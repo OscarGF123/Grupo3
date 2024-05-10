e4cfc7e1ea892da386c50c6f79bc41ecebd464de
Oscar <oscarhappy456@gmail.com>
2024-05-10 16:38:46 -0500

    Se ha agredado el login version 1

---
+++ mainLogin.js
@@ -0,0 +1,11 @@
+const $submit = document.getElementById("submit"),
+        $password = document.getElementById("Password"),
+        $username = document.getElementById("Username"),
+        $visible = document.getElementById("visble");
+
+        document.addEventListener("change"), (e) => {
+            if (e.target === $visible) {
+                if ($visible.checked === false) $password.type = "password";
+                else $password.type = "text";
+            }
+        }
